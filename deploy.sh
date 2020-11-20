f5ee777e26502ccc9d7ba60188580eebd472db1b
bharathpurvam <69152242+bharathpurvam@users.noreply.github.com>
2020-11-20 19:28:03 +1100

    added deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
-docker build -t bpurvam/multi-client -f ./client/Docker ./client
-docker build -t bpurvam/multi-server -f ./server/Docker ./server
-docker build -t bpurvam/multi-worker -f ./worker/Docker ./worker
-docker push bpurvam/multi-client
-docker push bpurvam/multi-server
-docker push bpurvam/multi-worker
+docker build -t bpurvam/multi-client:latest -t bpurvam/multi-client:$SHA -f ./client/Docker ./client
+docker build -t bpurvam/multi-server:latest -t bpurvam/multi-server:$SHA -f ./server/Docker ./server
+docker build -t bpurvam/multi-worker:latest -t bpurvam/multi-worker:$SHA -f ./worker/Docker ./worker
+
+docker push bpurvam/multi-client:latest
+docker push bpurvam/multi-server:latest
+docker push bpurvam/multi-worker:latest
+
+docker push bpurvam/multi-client:$SHA
+docker push bpurvam/multi-server:$SHA
+docker push bpurvam/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=bpurvam/multi-server
+kubectl set image deployments/client-deployment client=bpurvam/multi-client:$SHA
+kubectl set image deployments/server-deployment server=bpurvam/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=bpurvam/multi-worker:$SHA
